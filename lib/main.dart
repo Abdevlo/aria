103742195e255dbf2df4a3dae70b52ce04bc15f8
Abdevlo <abdallahmuath0@gmail.com>
2023-06-09 11:47:53 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,183 @@
 import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+import 'objectgestureplacement.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: InitialPage(),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/second') {
+          return MaterialPageRoute(builder: (context) => SecondPage());
+        }
+        return null;
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class InitialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Image.asset(
+              'assets/image/logo.png',
+              width: 150,
+              height: 50,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Text(
+              'Augmented Reality',
+              style: TextStyle(
+                fontFamily: 'NexaHeavy',
+                fontSize: 24,
+                color: Colors.black,
+              ),
+            ),
+            const Text(
+              'The Future is here',
+              style: TextStyle(
+                fontFamily: 'Nexa',
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/second');
+              },
+              child: Container(
+                width: 150,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Proceed',
+                    style: TextStyle(
+                      fontFamily: 'Nexa',
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:
+          Colors.transparent, 
+      appBar: AppBar(
+        title: const Text(
+          'Experience RealAR',
+          style: TextStyle(
+            fontFamily: 'NexaHeavy',
+            fontSize: 28,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+        ),
+        backgroundColor: Colors
+            .transparent, 
+        elevation: 0, 
+      ),
+      // body: const Center(
+      //   child: Text(
+      //     'This is the second page',
+      //     style: TextStyle(
+      //       fontFamily: 'Nexa',
+      //       fontSize: 18,
+      //       color: Color.fromARGB(255, 255, 255, 255),
+      //     ),
+      //   ),
+      // ),
+      body: ObjectGesturesWidget(), // Replace the Center widget with ObjectGesturesWidget
+    );
+  }
+}
+
+// class SecondPage extends StatefulWidget {
+//   @override
+//   _SecondPageState createState() => _SecondPageState();
+// }
+
+// class _SecondPageState extends State<SecondPage> {
+//   late CameraController _controller;
+//   late Future<void> _initializeControllerFuture;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _setupCamera();
+//   }
+
+//   Future<void> _setupCamera() async {
+//     final cameras = await availableCameras();
+//     final firstCamera = cameras.first;
+
+//     _controller = CameraController(firstCamera, ResolutionPreset.max);
+//     _initializeControllerFuture = _controller.initialize();
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<void>(
+//       future: _initializeControllerFuture,
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           return Scaffold(
+//             backgroundColor: Colors.transparent,
+//             appBar: AppBar(
+//               title: const Text(
+//                 'Experience Reality',
+//                 style: TextStyle(
+//                   fontFamily: 'NexaHeavy',
+//                   fontSize: 28,
+//                   color: Color.fromARGB(255, 255, 255, 255),
+//                 ),
+//               ),
+//               backgroundColor: Colors.transparent,
+//               elevation: 0,
+//             ),
+//             body: Center(
+//               child: CameraPreview(_controller),  
+//             ),
+//           );
+//         } else {
+//           return Scaffold(
+//             body: Center(
+//               child: CircularProgressIndicator(),
+//             ),
+//           );
+//         }
+//       },
+//     );
+//   }
+// }
